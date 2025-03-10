fab1763b0809533848cc72a7915c720b69208b82
leejuhi <anim0203@naver.com>
2025-03-10 22:51:52 +0900

    feat: 캘린더 만들기
    만들기

---
+++ calendar.js
@@ -0,0 +1,59 @@
+const date = new Date();
+const renderCalendar = () => {
+  const currentYear = date.getFullYear();
+  const currentMonth = date.getMonth();
+
+  document.querySelector(".today").textContent = `${currentYear}년 ${
+    currentMonth + 1
+  }월`;
+
+  const prevLast = new Date(currentYear, currentMonth, 0);
+  const currentLast = new Date(currentYear, currentMonth + 1, 0);
+
+  const prevLastDay = prevLast.getDay();
+  const prevLastDate = prevLast.getDate();
+  const currentLastDay = currentLast.getDay();
+  const currentLastDate = currentLast.getDate();
+
+  const prevDates = [];
+  const currentDates = [...Array(currentLast.getDate() + 1).keys()].slice(1);
+  const nextDates = [];
+
+  if (prevLastDay !== 6) {
+    for (let i = 0; i < prevLastDay + 1; i++) {
+      prevDates.unshift(prevLast.getDate() - i);
+    }
+  }
+
+  for (let i = 1; i < 7 - currentLastDay; i++) {
+    nextDates.push(i);
+  }
+
+  const dates = prevDates.concat(currentDates, nextDates);
+  dates.forEach((date, i) => {
+    const firstDate = prevDates.length;
+    const lastDateIndex = prevDates.length + currentDates.length - 1;
+    const condition =
+      i >= firstDate && i < lastDateIndex + 1 ? `current` : `other`;
+    dates[
+      i
+    ] = `<div class="date"><span class="${condition}">${date}</span></div>`;
+  });
+
+  document.querySelector(".dates").innerHTML = dates.join("");
+};
+
+renderCalendar();
+
+const prevMonth = () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+};
+const nextMonth = () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+};
+const goCurrentMonth = () => {
+  date.setMonth(new Date().getMonth());
+  renderCalendar();
+};
